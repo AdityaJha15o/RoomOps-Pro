caf891b726349f4f58bee485f1aa8a06e454e18b
DarkTrace <adityajhasusari@gmail.com>
2026-05-23 00:14:30 +0530

    fixed allowed hosts

--- roomops/settings.py
+++ roomops/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_5dr_k)2jak&p(4*%n64jr=b008b)rersird1mwudd^j&h*%&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roomops-pro.onrender.com']
 
 
 # Application definition
